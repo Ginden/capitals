9714503cc17b53bb5027bcdeab1542bc4f8aec28
Michał Wadas <ext-michal.wadas@here.com>
2016-12-02 16:47:24 +0100

    1.2.0

--- index.source.js
+++ index.source.js
@@ -1,22 +1,39 @@
 var details = $$$JSON_PLACEHOLDER$$$;
+
+var _ = require('lodash');
 details.forEach(function(obj) {
     Object.freeze(obj);
     Object.freeze(obj.geometry);
     Object.freeze(obj.geometry.coordinates);
     Object.freeze(obj.properties);
 });
+
+const euCountries = new Set([
+    'Austria', 'Belgium', 'Bulgaria', 'Croatia',
+    'Cyprus', 'Czech Republic', 'Denmark', 'Estonia',
+    'Finland', 'France', 'Germany', 'Greece',
+    'Hungary', 'Ireland', 'Italy', 'Latvia',
+    'Lithuania', 'Luxembourg', 'Malta', 'Netherlands',
+    'Poland', 'Portugal', 'Romania', 'Slovakia',
+    'Slovenia', 'Spain', 'Sweden', 'United Kingdom'
+]);
 module.exports = {
     rawData: details,
     findByCountryMatch: function(countryName) {
-        return details.filter(function(country) {
+        return Object.freeze(details.filter(function(country) {
             return country.details.country.match(countryName);
-        })
+        }));
     },
-    findByContinent: function(_continent) {
+    findByContinent: _.memoize(function(_continent) {
         var continent = String(_continent).toLowerCase().replace(' ', '-');
         if (continent === 'australia') continent = 'oceania';
-        return details.filter(function(country) {
+        return Object.freeze(details.filter(function(country) {
             return country.properties.continent === continent;
-        });
-    }
+        }));
+    }),
+    eu28: _.once(function() {
+        return Object.freeze(this.findByContinent('europe').filter(function(country) {
+            return euCountries.has(country.properties.country);
+        }));
+    })
 };
